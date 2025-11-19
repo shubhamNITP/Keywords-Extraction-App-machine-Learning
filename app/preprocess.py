c2cd9cefd556ae80f40f3521dc1ed9d5da98381d
shubhamNITP <shubhkr231@gmail.com>
2025-11-19 10:37:03 +0530

    Add punktabr

--- app/preprocess.py
+++ app/preprocess.py
@@ -5,15 +5,23 @@ from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 
 # Ensure required nltk files exist
-for resource in ["stopwords", "punkt"]:
+for resource in ["stopwords", "punkt", "punkt_tab"]:
     try:
-        nltk.data.find(f"corpora/{resource}" if resource == "stopwords" else f"tokenizers/{resource}")
+        if resource == "stopwords":
+            nltk.data.find("corpora/stopwords")
+        elif resource == "punkt":
+            nltk.data.find("tokenizers/punkt")
+        elif resource == "punkt_tab":
+            nltk.data.find("tokenizers/punkt_tab")
     except LookupError:
         nltk.download(resource)
 
 stop_words = set(stopwords.words('english'))
-extra_words = ["fig","figure","image","sample","using","show","result","large","also",
-               "one","two","three","four","five","six","seven","eight","nine"]
+extra_words = [
+    "fig","figure","image","sample","using","show",
+    "result","large","also","one","two","three","four",
+    "five","six","seven","eight","nine"
+]
 stop_words.update(extra_words)
 
 stemmer = PorterStemmer()
